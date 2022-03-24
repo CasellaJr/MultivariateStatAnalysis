a2f2a8ca3f40bc512e60000c85257a49db927f2a
LPaletto <lorenzo.paletto@unito.it>
2022-03-24 19:34:41 +0100

    vediamo_se_va

--- problemSet1.R
+++ problemSet1.R
@@ -11,11 +11,74 @@ rm(list=ls())
 
 ## US air pollution data
 
-usair<-read.table("data/usair.txt",header=TRUE) 
+usair <- read.table("data/usair.txt", header = TRUE)
+# converto a dataframe
+DF <- as.data.frame(usair)
+head(DF)
 
-head(usair)
-dim(usair)
 
+# cancello la colonna da ignorare
+DF$SO2 <- NULL
+
+# "Neg.Temp" "Manuf"  "Pop"   "Wind"  "Precip"  "Days" 
+#names(DF) <- paste("x",1:6,sep="")
+names(DF) <- c('x1','x2','x3','x4','x5','x6')
+attach(DF)
+
+# 1)
+# Media di ciascuna colonna
+x_bar <- colMeans(DF)
+# matrice di correlazione e covarianza
+Cors <- cor(DF)
+varcov <- cov(DF)
+
+# commentare le correlazioni
+
+# 2)
+
+boxplot(x1)
+# which(x1 < -71)   9 Miami
+boxplot(x2)
+# which(x2 > 1500)  11 29  Chicago(+3000) e philadelphia
+boxplot(x3)
+# which(x3 > 1600)  come sopra
+# parentesi su x2 e x3
+div = x2/x3 # fabbriche con >20 lavoratori per ogni 1k abitanti
+boxplot(div) # ci sono città con tante fabbriche in rapp agli abitanti
+which(div > 1.3) #5 27 31  Hartford Cleveland Providence
+
+boxplot(x4) # ok
+boxplot(x5)
+# which(x5 < 10)  1 23 Phoenix Alburquerque
+boxplot(x6)
+# which(x6<60) stessi due
+
+# calcolo la distanze di mahalanobis univariata
+
+DF_centr = sweep(DF,2,x_bar) # tolgo la media ad ogni riga
+names(DF_centr) <- c('m1','m2','m3','m4','m5','m6')
+attach(DF_centr)
+varianze = diag(varcov) # estraggo le varianze delle colonne
+for (i in 1:6){
+    DF_centr[,i] <- DF_centr[,i]/varianze[i]
+} # calcolo le distanze per ogni osservazione per ogni colonna
+
+# boxplot(m1) etc.. si vedono i boxplot di prima ma normalizzati
+# 3)
+
+
+
+
+# 4)
+# plotto i scatterplot per ogni coppia di variabili piazzando sui grafici
+# l'indice dell'istanza
+n <- dim(DF)[1]
+the.labels <- 1:n
+pairs(DF, panel = function(x, y) text(x, y, labels = the.labels))
+
+# le istanze che compaiono più volte come outlier multivariati
+# sono la 11 e la 29, Chicago e Philadelphia, le stesse che erano
+# outlier nelle analisi di x2 e x3 univariate
 ##############################################
 ##############################################
 
