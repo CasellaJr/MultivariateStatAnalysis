5229a1e13117af57f3738017d2984a657027dc1b
Bruno Casella <casella0798@gmail.com>
2022-03-26 14:17:34 +0100

    punti 3,5,6 dell'esercizio1

--- problemSet1.R
+++ problemSet1.R
@@ -19,7 +19,7 @@ head(DF)
 
 # cancello la colonna da ignorare
 DF$SO2 <- NULL
-
+head(DF)
 # "Neg.Temp" "Manuf"  "Pop"   "Wind"  "Precip"  "Days" 
 #names(DF) <- paste("x",1:6,sep="")
 names(DF) <- c('x1','x2','x3','x4','x5','x6')
@@ -32,16 +32,21 @@ x_bar <- colMeans(DF)
 Cors <- cor(DF)
 varcov <- cov(DF)
 
+x_bar
+Cors
+varcov
 # commentare le correlazioni
-
+'
+COMMENTI
+'
 # 2)
 
 boxplot(x1)
-# which(x1 < -71)   9 Miami
+which(x1 < -71)   #9 Miami
 boxplot(x2)
-# which(x2 > 1500)  11 29  Chicago(+3000) e philadelphia
+which(x2 > 1500)  #11 29  Chicago(+3000) e philadelphia
 boxplot(x3)
-# which(x3 > 1600)  come sopra
+which(x3 > 1600)  #come sopra
 # parentesi su x2 e x3
 div = x2/x3 # fabbriche con >20 lavoratori per ogni 1k abitanti
 boxplot(div) # ci sono città con tante fabbriche in rapp agli abitanti
@@ -49,9 +54,9 @@ which(div > 1.3) #5 27 31  Hartford Cleveland Providence
 
 boxplot(x4) # ok
 boxplot(x5)
-# which(x5 < 10)  1 23 Phoenix Alburquerque
+which(x5 < 10)  #1 23 Phoenix Alburquerque
 boxplot(x6)
-# which(x6<60) stessi due
+which(x6<60) #stessi due
 
 # calcolo la distanze di mahalanobis univariata
 
@@ -64,10 +69,28 @@ for (i in 1:6){
 } # calcolo le distanze per ogni osservazione per ogni colonna
 
 # boxplot(m1) etc.. si vedono i boxplot di prima ma normalizzati
+
 # 3)
-
-
-
+install.packages("moments")
+library(moments)
+qqnorm(x1,pch=16,main="Q-Q plot for x1")
+qqline(x1,col="red") #quite normal
+skewness(x1)
+qqnorm(x2,pch=16,main="Q-Q plot for x2")
+qqline(x2,col="red") #right skew distribution
+skewness(x2)
+qqnorm(x3,pch=16,main="Q-Q plot for x3")
+qqline(x3,col="red") #right skew distribution
+skewness(x3)
+qqnorm(x4,pch=16,main="Q-Q plot for x4")
+qqline(x4,col="red") #normal
+skewness(x4)
+qqnorm(x5,pch=16,main="Q-Q plot for x5")
+qqline(x5,col="red") #little left skewness
+skewness(x5)
+qqnorm(x6,pch=16,main="Q-Q plot for x6")
+qqline(x6,col="red") #normal
+skewness(x6)
 
 # 4)
 # plotto i scatterplot per ogni coppia di variabili piazzando sui grafici
@@ -79,6 +102,34 @@ pairs(DF, panel = function(x, y) text(x, y, labels = the.labels))
 # le istanze che compaiono più volte come outlier multivariati
 # sono la 11 e la 29, Chicago e Philadelphia, le stesse che erano
 # outlier nelle analisi di x2 e x3 univariate
+
+
+# 5-6)
+## multivariate normality
+n<-dim(DF)[1]
+n
+p<-dim(DF)[2]
+p
+X<-DF
+# one<-rep(1,n)
+# D<-as.matrix(X-one%*%t(bar.x))
+D<-scale(DF,scale=F)
+
+d<-rep(0,n)
+for(i in 1:n) d[i]<-t(D[i,])%*%solve(varcov)%*%D[i,]
+help("mahalanobis")
+d
+mahalanobis(X,center=colMeans(X),cov=var(X))
+#                                     # the same
+plot(d)
+
+plot(qchisq(ppoints(d),df=p),sort(d),main="Chisq Q-Q plot of Mahalanobis distance",
+     xlab="Theoretical Quantiles",ylab="Sample Quantiles")
+abline(0,1)
+text(qchisq(ppoints(d),df=p),sort(d),label=order(d),
+     pos=4,cex=0.5,offset=0.3)
+#outliers 1, 9, 11. L'11 era stato identificato anche in precedenza
+
 ##############################################
 ##############################################
 
